95c09dd461974c7620d52a413db8a7de0a8cf061
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-11 12:15:06 +0545

    template ready

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <Router>
+          <Routes>
+            <Route index element={} />
+            <Route path="/*" element={} />
+            <Route path="contact" element={} />
+          </Routes>
+      </Router> */}
+      "hello world"
+    </div>
+  )
+}
+
+export default App
